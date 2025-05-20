564d2825473ac67ea8b677318599cfbeee27ef95
Javiermartbosq <javiermartini156@gmail.com>
2025-05-20 12:39:07 +0200

    comentarios

--- AplicacionAlquileres/src/gui/InicioSesion.java
+++ AplicacionAlquileres/src/gui/InicioSesion.java
@@ -22,15 +22,28 @@ import bdd.DbUsuario;
 import modelo.Sesion;
 import modelo.Usuario;
 
+/**
+ * @author Pyto_Grupo_D
+ * @version 1.0
+ * 
+ * Ventana que permite el inicio de sesión a los usuarios de la aplicación.
+ * Pide introducir un DNI y contraseña, que son validadas. Si la autenticación
+ * es correcta redirige al menú princial.
+ * 
+ */
 public class InicioSesion extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	/**Panel principal de la ventana*/
 	private JPanel contentPane;
+	/**Campo de texto para ingresar el DNI*/
 	private JTextField txtDni;
+	/**Campo de texto para ingresar la contraseña*/
 	private JTextField txtContrasea;
 
 	/**
-	 * Launch the application.
+	 * Método principal. Lanza la aplicación y muestra el inicio de sesión.
+	 * 
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -46,7 +59,7 @@ public class InicioSesion extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Constructor que inicializa el inicio de sesión y sus partes.
 	 */
 	public InicioSesion() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -60,21 +73,18 @@ public class InicioSesion extends JFrame {
 		lblIniciarSesin.setBounds(164, 53, 100, 15);
 		contentPane.add(lblIniciarSesin);
 		
-		// Campo DNI
 		txtDni = new JTextField();
 		txtDni.setText("DNI");
 		txtDni.setBounds(164, 80, 114, 19);
 		contentPane.add(txtDni);
 		txtDni.setColumns(10);
 		
-		// Evento para borrar el "placeholder" cuando el usuario hace clic
 		txtDni.addFocusListener(new FocusAdapter() {
 		    public void focusGained(FocusEvent evt) {
 		        if (txtDni.getText().equals("DNI")) {
 		        	txtDni.setText("");
 		        }
 		    }
-		    
 		    public void focusLost(FocusEvent evt) {
 		        if (txtDni.getText().isEmpty()) {
 		        	txtDni.setText("DNI");
@@ -82,21 +92,18 @@ public class InicioSesion extends JFrame {
 		    }
 		});
 		
-		// Campo Contraseña
 		txtContrasea = new JTextField();
 		txtContrasea.setText("Contraseña");
 		txtContrasea.setBounds(164, 111, 114, 19);
 		contentPane.add(txtContrasea);
 		txtContrasea.setColumns(10);
 		
-		// Evento para borrar el "placeholder" cuando el usuario hace clic
 		txtContrasea.addFocusListener(new FocusAdapter() {
 		    public void focusGained(FocusEvent evt) {
 		        if (txtContrasea.getText().equals("Contraseña")) {
 		        	txtContrasea.setText("");
 		        }
 		    }
-		    
 		    public void focusLost(FocusEvent evt) {
 		        if (txtContrasea.getText().isEmpty()) {
 		        	txtContrasea.setText("Contraseña");
@@ -104,12 +111,10 @@ public class InicioSesion extends JFrame {
 		    }
 		});
 		
-		// Botón de Enviar
 		JButton btnEnviar = new JButton("Enviar");
 		btnEnviar.setBounds(161, 144, 117, 25);
 		contentPane.add(btnEnviar);
 
-		// Acción del botón "Enviar"
 		btnEnviar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String dni = txtDni.getText();
@@ -120,13 +125,11 @@ public class InicioSesion extends JFrame {
 		                DbUsuario dbUsuario = new DbUsuario();
 		                Usuario usuario = dbUsuario.ver1Usuario(dni);
 		                if (usuario != null) {
-		                    Sesion.setUsuarioActivo(usuario); // Guardar el usuario en la sesión
+		                    Sesion.setUsuarioActivo(usuario); 
 
-		                    // Abrir ventana Menu
-		                    Menu menu = new Menu(); // Asegúrate de importar tu clase Menu
+		                    Menu menu = new Menu(); 
 		                    menu.setVisible(true);
 
-		                    // Cerrar ventana actual
 		                    dispose();
 		                } else {
 		                    JOptionPane.showMessageDialog(null, "Error al obtener los datos del usuario.");
